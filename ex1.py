cfd9bf6f8d2d36588da10c45028f45d48a51f232
Chloe Le <chloethuyduong16@gmail.com>
2018-02-11 12:39:26 +0000

    add instructions

--- ex1.py
+++ ex1.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
-import time
+#Create a program that asks the user to enter their name and their age. 
+# Print out a message addressed to them that tells them the year that 
+# they will turn 100 years old.
 
+#Extras:
+
+#Add on to the previous program by asking the user for another number 
+# and printing out that many copies of the previous message. 
+# (Hint: order of operations exists in Python)
+#Print out that many copies of the previous message on separate lines. 
+# (Hint: the string "\n is the same as pressing the ENTER button)
+
+import time
 
 name = input ("Give me your name: ")
 age = input ("Give me your age: ")
